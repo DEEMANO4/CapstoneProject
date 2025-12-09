6537b8d729012629901afbfd4a9b9adf90197b77
DEEMANO4 <apenadavid18@gmail.com>
2025-12-09 12:01:56 +0100

    Created required Views logic

--- AppointmentScheduler/booking/models.py
+++ AppointmentScheduler/booking/models.py
@@ -12,22 +12,10 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
-    
-
-class Appointment(models.Model):
-    appointment_id = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='appointments')
-    time_slot_id = models.IntegerField
-    appointment_date = models.DateTimeField()
-    status = models.CharField(max_length=20)
-    notes = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.service.name} for {self.user.username} on {self.start_time.strftime('%Y-%m-%d %H:%M')}"
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
+    employee_id = models.IntegerField()
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     specialization = models.CharField(max_length=100)
@@ -38,7 +26,7 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
 
-class Timeslot(models.Model):
+class TimeSlot(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='timeslots')
     date = models.DateField()
     start_time = models.TimeField()
@@ -50,6 +38,24 @@ class Timeslot(models.Model):
 
     def __str__(self):
         return f"{self.employee.name} - {self.date} {self.start_time}-{self.end_time}"
+
+class Appointment(models.Model):
+    appointment_id = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='appointments')
+    service_offered = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='service')
+    employee_available = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='employees available')
+    # service_id = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='service id')
+    time_slot = models.ForeignKey(Timeslot, on_delete=models.CASCADE, related_name='timeslots_available' )
+    appointment_date = models.DateTimeField()
+    status = models.CharField(max_length=20)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.service.name} for {self.user.username} on {self.start_time.strftime('%Y-%m-%d %H:%M')}"
+
+
+
 
 # User = get_user_model
 
--- AppointmentScheduler/booking/views.py
+++ AppointmentScheduler/booking/views.py
@@ -1,3 +1,139 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy, reverse
+from django.http import JsonResponse
+import json
+
+from .models import Service, Appointment, Employee, TimeSlot, Notification
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'booking/home.html'
+
+class ServiceListeView(ListView):
+    model = Service
+    template_name = 'booking/service_list.html'
+    context_object_name = 'services'
+
+class AppointmentListView(LoginRequiredMixin, ListView):
+    model = Appointment
+    template_name = 'booking/appointment_list.html'
+    context_object_name = 'appointments'
+
+    def qet_queryset(self):
+        return Appointment.objects.filter(user=self.request.user).order_by('start_time')
+    
+class AppointmentDetailView(LoginRequiredMixin, DetailView):
+    model = Appointment
+    template_name = 'booking/appointment_details.html'
+    context_object_name = 'appointment' 
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+    
+class AppointmentCancelView(LoginRequiredMixin, DeleteView):
+    model = Appointment
+    template_name = 'booking/cancel_appointment.html'
+    success_url = reverse_lazy('booking:my_appointments')
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+    
+class AppointmentCreateView(LoginRequiredMixin, CreateView):
+    model = Appointment
+    fields = ['service_offered', 'employee_available,', 'time_slot']
+    template_name = 'booking/appoinment_booking.html' 
+    success_url = reverse_lazy('booking:my_appointments')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.time_slot.is_booked = True
+        form.instance.time_slot.save()
+        return super().form_valid(form)
+    
+class AppointmentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Appointment
+    fields = ['service_offered', 'employee_available,', 'time_slot']
+    template_name = 'booking/appoinment_booking.html' 
+    success_url = reverse_lazy('booking:my_appointments')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.time_slot.is_booked = True
+        form.instance.time_slot.save()
+        return super().form_valid(form)
+    
+def load_available_timeslots(request):
+    employee_id = request.GET,get('emplyee_id')
+    service_id = request.GET.get('service_id')
+
+    timeslots = TimeSlot.objects.filter(employee_id=employee_id, is_booked= False, service_id=service_id).order_by('start_time')
+
+    data = [{
+        'id': time_slot.id,
+        'start_time': time_slot.start_time.strftime('Y-%m-%d %H:%M'),
+        'end_time': time_slot.end_time.strftime('%Y-%m-%d %H:%M'),
+    } for time_slot in timeslots]
+
+    return JsonResponse(data, safe=False)
+
+class NotificationListView(LoginRequiredMixin, ListView):
+    model = Notification
+    template_name = 'booking/notification_list.html'
+    context_object_name ='notifications'
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user).order_by('-created_at')
+    
+def mark_notification_as_read(request, pk):
+    notification = get_object_or_404(Notification, pk=pk, user=request.user)
+    if request.method ==  'POST':
+        notification.is_read = True
+        notification.save()
+
+        return redirect('booking:notification_list.html')
+    
+
+class EmployeeCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Employee
+    fields = ['name', 'specialization']
+    template_name = 'booking/employee_form.html'
+    success_url = reverse_lazy('booking:employee_list')
+
+    def test_func(self):
+        return self.request.user.is_staff
+    
+class EmployeeListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Employee
+    template_name = 'booking/employee_list.html'
+    context_object_name = 'employees'
+
+    def get_queryset(self):
+        return self.models.objects.filter(user=self.request.user).order_by()
+    
+class EmployeeDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Employee
+    template_name = 'booking/employee_details.html'
+    context_object_name = 'employees'
+
+    def get_queryset(self):
+        return self.models.objects.filter(user=self.request.user).order_by()
+    
+class EmployeeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Employee
+    template_name = 'booking/employee_removed.html'
+    success_url = reverse_lazy('booking:my_appointments')
+
+    def get_queryset(self):
+        return self.models.objectss.filter(user=self.request.user).order_by()
+    
+class EmployeeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+     model = Employee
+     fields = ['name', 'specialization']
+     template_name = 'booking/employee_form.html'
+     success_url = reverse_lazy('booking:employee_list')
+
+     def test_func(self):
+        return self.request.user.is_staff
